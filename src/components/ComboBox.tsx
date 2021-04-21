ffa24becfc763b665f7694966e118c7b15e58e58
Bryan <bryan.cedeo@gmail.com>
2021-04-21 01:36:18 -0500

    cambios para arreglar bugs visuales

--- src/components/ComboBox.tsx
+++ src/components/ComboBox.tsx
@@ -1,16 +1,16 @@
 import React from 'react'
-import RNPickerSelect from 'react-native-picker-select';
+import RNPickerSelect, { PickerStyle } from 'react-native-picker-select';
 
 import { IndexPath, Layout, Select, SelectItem } from '@ui-kitten/components';
 import { uiKiten } from '../themes/ComponentStyles';
 import { Control, Controller, FieldValues } from 'react-hook-form';
-import { StyleProp, TextStyle } from 'react-native';
+import { Image, StyleProp, TextStyle, View } from 'react-native';
 
 
 interface PropsSelect {
     name: string,
     control: Control<FieldValues>,
-    style?: StyleProp<TextStyle>,
+    style?: PickerStyle,
     comboData: Array<{ label: string, value: string }>
 }
 
@@ -21,30 +21,25 @@ export const ComboBox = ({name, control, style, comboData}:PropsSelect) => {
             <Controller
                 control={control}
                 render={({ field: { onChange, onBlur, value } }) => (
-                    /*<Layout style={style? style :uiKiten.container} level='1'>
-                        <Select
-                            size="large"
-                            selectedIndex={value}
-                            onSelect={index => onChange(index)}
-                            onBlur={onBlur}
-                        >
-                            {
-                                comboData.map((option, index)=>{
-                                    return (
-                                        <SelectItem 
-                                            title={option} 
-                                            descriptor={{multiSelect: false, index: (new IndexPath(index))}} 
-                                            key={index} 
-                                        />)
-                                })
-                            }
-                        </Select>
-                    </Layout>*/
-                    <RNPickerSelect
-                        onValueChange={(value) => onChange(value)}
-                        value={value}
-                        items={comboData}
-                    />
+                    <View
+                        style={{
+                            justifyContent: 'center'
+                        }}
+                    >
+                        <RNPickerSelect
+                            onValueChange={(value) => onChange(value)}
+                            useNativeAndroidPickerStyle={false}
+                            style={ style? style : {inputAndroid: {color: 'black', backgroundColor: '#fff', paddingHorizontal: 20} }}
+                            value={value}
+                            items={comboData}
+                        />
+                        <Image style={{
+                            position: 'absolute',
+                            right: 15,
+                            width: 15,
+                            height: 15,
+                        }} source={require('../resources/down.jpg')} />
+                    </View>
                 )}
                 name={name}
                 rules={{ required: true }}
